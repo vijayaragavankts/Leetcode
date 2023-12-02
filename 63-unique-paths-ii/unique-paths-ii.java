c873ac9d8058156f811cd77e8e70a8bdb179fc3b
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2023-12-02 20:10:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (28.06%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -3,6 +3,9 @@ class Solution {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         int[][] dp = new int[m][n];
+        for(int[] arr:dp){
+            Arrays.fill(arr,-1);
+        }
         return f(m-1,n-1,obstacleGrid,dp);
     }
     public int f(int i,int j, int[][] nums, int[][] dp){
@@ -15,7 +18,7 @@ class Solution {
         if(i == 0 && j == 0){
             return 1;
         }
-        if(dp[i][j] != 0){
+        if(dp[i][j] != -1){
             return dp[i][j];
         }
         int left = f(i,j-1,nums,dp);
