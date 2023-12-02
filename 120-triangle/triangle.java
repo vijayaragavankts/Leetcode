fd989c6ef3724713f7a10dde44859ea807cb66e9
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2023-12-02 20:35:47 +0530

    Time: 4 ms (47.31%) | Memory: 44.4 MB (15.51%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -12,12 +12,6 @@ class Solution {
                 }
             }
         }
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                System.out.print(dp[i][j] + " ");
-            }
-            System.out.println();
-        }
         return dp[0][0];
     }
 }
