aaa7dcd5c522136fa9adbb32205317bb32eaa0f5
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-02-11 14:10:07 +0530

    Time: 4 ms (28.16%) | Memory: 47 MB (72.11%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,38 +1,44 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        List<List<Integer>> adj = new ArrayList();
-        for (int i = 0; i < n; i++) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++){
             adj.add(new ArrayList());
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1 && i != j) {
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i != j && isConnected[i][j] == 1){
                     adj.get(i).add(j);
                     adj.get(j).add(i);
                 }
             }
         }
-        System.out.println(adj);
-        int[] vis = new int[n];
+
         Queue<Integer> queue = new LinkedList<>();
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (vis[i] == 0) {
-                vis[i] = 1;
-                queue.offer(i);
-                while(!queue.isEmpty()){
-                    int current = queue.poll();
-                    for(int j:adj.get(current)){
-                        if(vis[j] == 0){
-                            vis[j] = 1;
-                            queue.offer(j);
-                        }
-                    }
-                }
+        int[] visited = new int[n];
+
+        for(int i=0;i<n;i++){
+            if(visited[i] == 0){
+                bfs(i,visited,adj,queue);
                 count++;
             }
         }
         return count;
+    }
+    public void bfs(int node, int[] visited, List<List<Integer>> adj, Queue<Integer> queue){
+        visited[node] = 1;
+        queue.offer(node);
+
+        while(!queue.isEmpty()){
+            int val = queue.poll();
+            for(int element:adj.get(val)){
+                if(visited[element] == 0){
+                    queue.offer(element);
+                    visited[element] = 1;
+                }
+            }
+        }
     }
 }
