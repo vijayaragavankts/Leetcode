89b82e5a6b86552965bb603543b7d95825fac284
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-02-13 18:18:27 +0530

    Time: 2 ms (76.64%) | Memory: 42.5 MB (40.50%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -12,17 +12,13 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int count_fresh = 0;
         int[][] visited = new int[m][n];
         Queue<Pair> queue = new LinkedList<>();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j] == 2){
-                    visited[i][j] = 2;
-                    queue.offer(new Pair(i,j,0));
-                }
-                if(grid[i][j] == 1){
                     visited[i][j] = 1;
+                    queue.offer(new Pair(i,j,0));
                 }
             }
         }
@@ -38,29 +34,20 @@ class Solution {
             for(int i=0;i<4;i++){
                 int r = row + drow[i];
                 int c = col + dcol[i];
-                if(r >= 0 && c >= 0 && r < m && c < n && visited[r][c] != 2 && grid[r][c] == 1){
+                if(r >= 0 && c >= 0 && r < m && c < n && visited[r][c] == 0 && grid[r][c] == 1){
                     queue.offer(new Pair(r,c,time+1));
-                    visited[r][c] = 2;
+                    visited[r][c] = 1;
                 }
             }
-        }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                System.out.print(visited[i][j] + " ");
-            }
-            System.out.println();
-        }   
+        } 
         int count = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(visited[i][j] == 1){
-                    count++;
+                if(visited[i][j] == 0 && grid[i][j] == 1){
+                    return -1;
                 }
             }
-        }   
-        if(count > 0){
-            return -1;
-        }
+        }  
         return t;
     }
 }
