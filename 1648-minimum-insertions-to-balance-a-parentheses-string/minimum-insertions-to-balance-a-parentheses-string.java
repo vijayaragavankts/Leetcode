c565335616459b4b260769aaa427ca565a7ac68c
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-06-09 17:31:11 +0530

    Time: 23 ms (33.01%) | Memory: 45.7 MB (5.77%) - LeetSync

---
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minInsertions(String s) {
+        s = s.replace("))","}");
+        System.out.println(s);
+        int invalid = 0;
+        int stack = 0;
+        for(char ch:s.toCharArray()){
+            if(ch == '('){
+                stack++;
+            }
+            else{
+                if(ch == ')'){
+                    invalid++;
+                }
+                if(stack == 0){
+                    invalid++;
+                }
+                else{
+                    stack--;
+                }
+            }
+            
+        }
+        return invalid + (stack * 2) ;
+    }
+}
