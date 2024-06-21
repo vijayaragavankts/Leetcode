39656eea6a9eaa0efbb983756251b87bd978b955
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-06-21 16:45:41 +0530

    Time: 4 ms (48.10%) | Memory: 51.4 MB (21.65%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -3,28 +3,33 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int count = 0;
+        int[][] vis = new int[m][n];
+        for(int i=0;i<m;i++){
+            Arrays.fill(vis[i],-1);
+        }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j] == '1'){
-                    dfs(i,j,grid);
+                if(grid[i][j] == '1' && vis[i][j] == -1){
                     count++;
+                    dfs(i,j,grid,vis);
                 }
             }
         }
         return count;
     }
-    public void dfs(int i, int j, char[][] grid){
+    public void dfs(int i, int j, char[][] grid, int[][] vis){
         int m = grid.length;
         int n = grid[0].length;
-        grid[i][j] = '0';
+        vis[i][j] = 1;
         int[] drow = {-1,0,1,0};
         int[] dcol = {0,1,0,-1};
         for(int x=0;x<4;x++){
             int row = i + drow[x];
             int col = j + dcol[x];
-            if(row >= 0 && col >= 0 && row < m && col < n && grid[row][col] == '1'){
-                dfs(row,col,grid);
+            if(row >= 0 && col >= 0 && row < m && col < n && grid[row][col] == '1' && vis[row][col] == -1){
+                dfs(row,col,grid,vis);
             }
         }
+        return;
     }
 }
