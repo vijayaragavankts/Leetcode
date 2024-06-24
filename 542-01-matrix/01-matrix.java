7678a9974bb256e1dbfcd6afe8cf1f874cce9da1
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-06-24 13:31:43 +0530

    Time: 16 ms (32.86%) | Memory: 47.9 MB (16.74%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -8,7 +8,6 @@ class Pair{
         this.dis = dis;
     }
 }
-
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int m = mat.length;
@@ -27,7 +26,6 @@ class Solution {
                 }
             }
         }
-
         while(!queue.isEmpty()){
             int r = queue.peek().row;
             int c = queue.peek().col;
@@ -48,27 +46,5 @@ class Solution {
         }
 
         return res;
-    }
-    public void bfs(int i, int j, int[][] mat, int[][] vis, int[][] res){
-        int m = mat.length;
-        int n = mat[0].length;
-        vis[i][j] = 1;
-        Queue<Pair> queue = new LinkedList<>();
-        queue.offer(new Pair(i,j,1));
-        res[i][j] = 1;
-        while(!queue.isEmpty()){
-            int r = queue.peek().row;
-            int c = queue.peek().col;
-            queue.poll();
-            int[] drow = {-1,0,1,0};
-            int[] dcol = {0,1,0,-1};
-            for(int x=0;x<4;x++){
-                int row = r + drow[x];
-                int col = c + dcol[x];
-                if(row >= 0 && col >= 0 && row < m && col < n && mat[row][col] == 1){
-
-                }
-            }
-        }
     }
 }
