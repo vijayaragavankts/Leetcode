8d0095b4f7327afcdb4d60bf5d0cfbdcfe2438d8
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-07-01 19:53:48 +0530

    Time: 60 ms (5.06%) | Memory: 46.5 MB (5.06%) - LeetSync

---
+++ 2669-find-the-substring-with-maximum-cost/find-the-substring-with-maximum-cost.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maximumCostSubstring(String s, String chars, int[] vals) {
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<chars.length();i++){
+            map.put(chars.charAt(i),vals[i]);
+        }
+        int n = s.length();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            if(map.containsKey(ch)){
+                arr[i] = map.get(ch);
+            }
+            else{
+                arr[i] = ch - 96;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+        // kadane's method
+        int sum = 0;
+        int max = Integer.MIN_VALUE;;
+        for(int val:arr){
+            sum += val;
+            if(sum > max){
+                max = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return max > 0 ? max : 0;
+    }
+}
