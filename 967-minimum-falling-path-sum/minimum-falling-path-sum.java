a5d8ef006a89c2f97ed872d1684a9baf7efee5cd
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-07-22 17:47:36 +0530

    Time: 69 ms (5.16%) | Memory: 44.8 MB (91.04%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,33 +1,32 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+        int n = matrix.length;
         int res = Integer.MAX_VALUE;
+        
         for(int i=0;i<n;i++){
-            int[][] dp = new int[m][n];
+            int[][] dp = new int[n][n];
             for(int[] arr:dp){
                 Arrays.fill(arr,(int)1e9);
             }
-            int ans = f(0,i,m-1,matrix,dp);
+            int ans = f(0,i,matrix,dp);
             res = Math.min(res,ans);
         }
         return res;
     }
-    public int f(int i, int j, int m, int[][] matrix, int[][] dp){
-        if(j < 0 || j > m || i > m){
+    public int f(int i, int j, int[][] matrix, int[][] dp){
+        int n = matrix.length;
+        if(j < 0 || j >= n){
             return Integer.MAX_VALUE;
         }
-
-        if(i == m){
+        if(i == n-1){
             return matrix[i][j];
         }
-        
         if(dp[i][j] != (int)1e9){
             return dp[i][j];
         }
-        int left = f(i+1,j-1,m,matrix,dp);
-        int st = f(i+1,j,m,matrix,dp);
-        int right = f(i+1,j+1,m,matrix,dp);
-        return dp[i][j] = Math.min(st,Math.min(left,right)) + matrix[i][j];
+        int bottom = f(i+1,j,matrix,dp);
+        int bottom_left = f(i+1,j-1,matrix,dp);
+        int bottom_right = f(i+1,j+1,matrix,dp);
+        return dp[i][j] = Math.min(bottom,Math.min(bottom_left,bottom_right)) + matrix[i][j];
     }
 }
