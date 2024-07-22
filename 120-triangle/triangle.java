a8a7d5503482db87905482e2066a89ce430f62a0
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-07-22 17:27:23 +0530

    Time: 1 ms (99.54%) | Memory: 44.5 MB (18.05%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,17 +1,24 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[][] dp = new int[n+1][n+1];
-        for(int i=n-1;i>=0;i--){
-            for(int j=0;j<triangle.get(i).size();j++){
-                if(i == n-1){
-                    dp[i][j] = triangle.get(i).get(j);
-                }
-                else{
-                    dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i+1][j] , dp[i+1][j+1]);
-                }
-            }
+        int[][] dp = new int[n][n];
+        for(int[] arr:dp){
+            Arrays.fill(arr,-1);
         }
-        return dp[0][0];
+        return f(triangle,0,0,n-1,dp);
+    }
+    public int f(List<List<Integer>> list, int i, int j, int n, int[][] dp){
+        if(j < 0 || j > list.get(i).size()){
+            return Integer.MAX_VALUE;
+        }
+        if(i == n){
+            return list.get(i).get(j);
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int down = f(list,i+1,j,n,dp);
+        int diagonal = f(list,i+1,j+1,n,dp);
+        return dp[i][j] = Math.min(down,diagonal) + list.get(i).get(j);
     }
 }
