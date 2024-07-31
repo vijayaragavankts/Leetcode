1686b3a9d4a678ee1019c9b7b701f19de409ce01
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-07-31 11:19:06 +0530

    Time: 263 ms (5.00%) | Memory: 124.7 MB (5.13%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,35 +1,37 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int total = 0;
+        int sum = 0;
         for(int val:nums){
-            total += val;
+            sum += val;
         }
-        if(total % 2 != 0){
+        if(sum % 2 != 0){
             return false;
         }
+        int target = sum / 2;
         int n = nums.length;
-        int[][] dp = new int[n][(total/2)+1];
+        int[][] dp = new int[n][50000];
         for(int[] arr:dp){
             Arrays.fill(arr,-1);
         }
-        return f(nums,n-1,total/2,dp);
+        return f(nums,n-1,target,dp);
     }
-    public boolean f(int[] nums, int ind, int target,int[][] dp){
-        if(ind == 0){
-            return target == nums[ind];
-        }
+    public boolean f(int[] nums, int i, int target,int[][] dp){
         if(target == 0){
             return true;
         }
-        if(dp[ind][target] != -1){
-            return dp[ind][target] == 1 ? true : false;
+        if(i == 0){
+            return nums[i] == target;
         }
-        boolean notTake = f(nums,ind-1,target,dp);
+        if(dp[i][target] != -1){
+            return dp[i][target] == 1 ? true:false;
+        }
+        boolean notTake = f(nums,i-1,target,dp);
         boolean take = false;
-        if(target >= nums[ind]){
-            take = f(nums,ind-1,target-nums[ind],dp);
+        if(target >= nums[i]){
+            take = f(nums,i-1,target-nums[i],dp);
         }
-        dp[ind][target] = take || notTake ? 1 : 0;
+        dp[i][target] = take || notTake ? 1 : 0;
         return take || notTake;
+
     }
 }
