038062c9deb338baa8bdf5767bdf6fef2c180fdd
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-08-01 18:53:14 +0530

    Time: 5 ms (10.24%) | Memory: 43 MB (16.91%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int n = details.length;
+        int res = 0;
+        List<Integer> list = new ArrayList<>();
+        for(String s:details){
+            int first = s.charAt(11)-'0';
+            int second = s.charAt(12)-'0';
+            list.add((first * 10) + second);
+        }
+        for(int val:list){
+            if(val > 60){
+                res++;
+            }
+        }
+        System.out.println(list);
+        return res;
+    }
+}
