59c5e857edf0b883b92faeb2b21578ce72cf3319
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-08-08 14:33:13 +0530

    Time: 10 ms (7.19%) | Memory: 42.6 MB (6.22%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String customSortString(String order, String s) {
+        Map<Character,Integer> map = new HashMap<>();
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        String res = "";
+        for(char ch:order.toCharArray()){
+            if(map.containsKey(ch)){
+                int count = map.get(ch);
+                while(count > 0){
+                    res += ch;
+                    count--;
+                }
+                map.remove(ch);
+            }
+        }
+        System.out.println(map);
+        System.out.println(res);
+        for(Map.Entry<Character,Integer> e:map.entrySet()){
+            int count = e.getValue();
+            char val = e.getKey();
+            while(count > 0){
+                res += val;
+                count--;
+            }
+        }
+        return res;
+    }
+}
