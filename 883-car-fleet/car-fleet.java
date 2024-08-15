8d6f7ef0c37748747932c8a7a4d785b93dd048fc
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-08-15 19:04:44 +0530

    Time: 1380 ms (5.05%) | Memory: 57.6 MB (62.49%) - LeetSync

---
+++ 883-car-fleet/car-fleet.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        double[][] cars = new double[n][2];
+        for(int i=0;i<n;i++){
+            cars[i][0] = position[i];
+            cars[i][1] = (double)(target - position[i]) / speed[i];
+        }
+        for(double[] a:cars){
+            System.out.println(Arrays.toString(a));
+        }
+        Arrays.sort(cars,(a,b)->Double.compare(b[0],a[0]));
+        for(double[] a:cars){
+            System.out.println(Arrays.toString(a));
+        }
+        double prev = 0;
+        int count = 0;
+        for(double[] arr:cars){
+            if(arr[1] > prev){
+                count++;
+                prev = arr[1];
+            }
+        }
+        return count;
+    }
+}
