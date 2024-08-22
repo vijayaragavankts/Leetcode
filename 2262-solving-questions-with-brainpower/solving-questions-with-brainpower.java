875ca770acc3c18091efaa985c2617456442929d
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-08-22 13:09:06 +0530

    Time: 14 ms (26.56%) | Memory: 123.8 MB (35.89%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -2,26 +2,24 @@ class Solution {
     long[] dp;
     public long mostPoints(int[][] questions) {
         int m = questions.length;
-        int n = questions[0].length;
         dp = new long[m];
         Arrays.fill(dp,-1);
         return f(questions,0,m);
     }
     public long f(int[][] mat, int i, int m){
         // base case
+
         if(i >= m){
             return 0;
         }
+
         if(dp[i] != -1){
             return dp[i];
         }
-        long pick = mat[i][0] + f(mat, i + mat[i][1] + 1, m);
 
-        long notPick = 0 + f(mat,i+1,m);
-       
-        
+        long notTake = f(mat,i+1,m);
+        long take = f(mat,i+mat[i][1]+1,m) + mat[i][0];
 
-        return dp[i] = Math.max(pick,notPick);
-
+        return dp[i] = Math.max(take,notTake);
     }
 }
