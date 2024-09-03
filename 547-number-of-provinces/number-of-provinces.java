d8f2b3786181ba526456afecea6b06a82e449195
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-09-03 22:00:02 +0530

    Time: 6 ms (9.67%) | Memory: 46.5 MB (91.08%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,53 +1,36 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
+        // adjacency list
         List<List<Integer>> adj = new ArrayList<>();
+        int n = isConnected.length;
+
         for(int i=0;i<n;i++){
             adj.add(new ArrayList());
         }
-
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(i != j && isConnected[i][j] == 1){
+                if(isConnected[i][j] == 1 && i != j){
                     adj.get(i).add(j);
                     adj.get(j).add(i);
                 }
             }
         }
-
+        boolean[] vis = new boolean[n];
         int count = 0;
-        int[] visited = new int[n];
         for(int i=0;i<n;i++){
-            if(visited[i] == 0){
-                dfs(i,visited,adj);
+            if(!vis[i]){
                 count++;
+                dfs(adj,vis,i);
             }
         }
-        
         return count;
     }
-    public void bfs(int node, int[] visited, List<List<Integer>> adj, Queue<Integer> queue){
-        visited[node] = 1;
-        queue.offer(node);
-
-        while(!queue.isEmpty()){
-            int val = queue.poll();
-            for(int element:adj.get(val)){
-                if(visited[element] == 0){
-                    queue.offer(element);
-                    visited[element] = 1;
-                }
-            }
-        }
-    }
-
-    public void dfs(int node, int[] visited, List<List<Integer>> adj){
-        visited[node] = 1;
+    public void dfs(List<List<Integer>> adj, boolean[] vis, int node){
+        vis[node] = true;
         for(int val:adj.get(node)){
-            if(visited[val] == 0){
-                dfs(val,visited,adj);
+            if(!vis[val]){
+                dfs(adj,vis,val);
             }
         }
-        return;
     }
 }
