7917f789689861111a0e5b8236cec83be1a2e1dc
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-10-18 13:14:10 +0530

    Time: 11 ms (76.77%) | Memory: 45.1 MB (85.20%) - LeetSync

--- 2492-length-of-the-longest-alphabetical-continuous-substring/length-of-the-longest-alphabetical-continuous-substring.java
+++ 2492-length-of-the-longest-alphabetical-continuous-substring/length-of-the-longest-alphabetical-continuous-substring.java
@@ -1,22 +1,17 @@
 class Solution {
     public int longestContinuousSubstring(String s) {
-        int maxLength = 1;
-        int currentLength = 1;
-        
-        // Iterate through the string starting from the second character
-        for (int i = 1; i < s.length(); i++) {
-            // Check if the current character is consecutive to the previous one
-            if (s.charAt(i) - s.charAt(i - 1) == 1) {
-                currentLength++;
-            } else {
-                // Reset currentLength if it's not continuous
-                currentLength = 1;
+        int n = s.length();
+        int res = 1;
+        int current = 1;
+        for(int i=1;i<n;i++){
+            if(s.charAt(i) - s.charAt(i-1) == 1){
+                current++;
             }
-            
-            // Update maxLength if currentLength is larger
-            maxLength = Math.max(maxLength, currentLength);
+            else{
+                current = 1;
+            }
+            res = Math.max(res,current);
         }
-        
-        return maxLength;
+        return res;
     }
 }
