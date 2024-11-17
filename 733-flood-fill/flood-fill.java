1bc27ea8eedda65d1822247da11fa04634abbd67
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-11-17 15:31:57 +0530

    Time: 2 ms (11.47%) | Memory: 44.9 MB (53.60%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,31 +1,36 @@
+class Pair{
+    int row;
+    int col;
+    public Pair(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+}
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int m = image.length;
         int n = image[0].length;
-        int original = image[sr][sc];
-        int[][] res = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                res[i][j] = image[i][j];
+        int original_color = image[sr][sc];
+        boolean[][] vis = new boolean[m][n];
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(sr,sc));
+        vis[sr][sc] = true;
+        while(!queue.isEmpty()){
+            int row = queue.peek().row;
+            int col = queue.peek().col;
+            queue.poll();
+            image[row][col] = color;
+            int[] drow = {-1,0,1,0};
+            int[] dcol = {0,1,0,-1};
+            for(int k=0;k<4;k++){
+                int r = row + drow[k];
+                int c = col + dcol[k];
+                if(r >= 0 && r < m && c >= 0 && c < n && image[r][c] == original_color && vis[r][c] == false){
+                    queue.offer(new Pair(r,c));
+                    vis[r][c] = true;
+                }
             }
         }
-        dfs(sr,sc,image,original,color,res);
-        return res;
-    }
-    public void dfs(int i, int j, int[][] image, int original, int color, int[][] res){
-        int m = image.length;
-        int n = image[0].length;
-        res[i][j] = color;
-        image[i][j] = -1;
-        int[] drow = {-1,0,1,0};
-        int[] dcol = {0,1,0,-1};
-        for(int x=0;x<4;x++){
-            int row = i + drow[x];
-            int col = j + dcol[x];
-            if(row >= 0 && col >= 0 && row < m && col < n && image[row][col] == original){
-                dfs(row,col,image,original,color,res);
-            }
-        }
-        return;
+        return image;
     }
 }
