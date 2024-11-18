5a9cb3f19667519f496f9b7d0193aa8aa6b397a2
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-11-18 20:54:53 +0530

    Time: 18 ms (20.28%) | Memory: 47.7 MB (22.52%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,50 +1,45 @@
 class Pair{
     int row;
     int col;
-    int dis;
-    public Pair(int row,int col, int dis){
+    int distance;
+    public Pair(int row, int col, int distance){
         this.row = row;
         this.col = col;
-        this.dis = dis;
+        this.distance = distance;
     }
 }
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
-        // bfs
         int[][] vis = new int[m][n];
         int[][] res = new int[m][n];
         Queue<Pair> queue = new LinkedList<>();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(vis[i][j] == 0){
-                    if(mat[i][j] == 0){
-                        queue.offer(new Pair(i,j,0));
-                        vis[i][j] = 1;
-                    }   
+                if(mat[i][j] == 0){
+                    queue.offer(new Pair(i,j,0));
                 }
             }
         }
         while(!queue.isEmpty()){
-            int r = queue.peek().row;
-            int c = queue.peek().col;
-            int d = queue.peek().dis;
-            res[r][c] = d;
+            int row = queue.peek().row;
+            int col = queue.peek().col;
+            int distance = queue.peek().distance;
             queue.poll();
             int[] drow = {-1,0,1,0};
             int[] dcol = {0,1,0,-1};
-            for(int x=0;x<4;x++){
-                int row = r + drow[x];
-                int col = c + dcol[x];
-                if(row >= 0 && col >= 0 && row < m && col < n && vis[row][col] == 0 && mat[row][col] == 1){
-                    queue.offer(new Pair(row,col,d+1));
-                    res[row][col] = d+1;
-                    vis[row][col] = 1;
+
+            for(int k=0;k<4;k++){
+                int r = row + drow[k];
+                int c = col + dcol[k];
+                if(r >= 0 && r < m && c >= 0 && c < n && vis[r][c] == 0 && mat[r][c] == 1){
+                    queue.offer(new Pair(r,c,distance+1));
+                    vis[r][c] = 1;
+                    res[r][c] = distance + 1;
                 }
             }
         }
-
         return res;
     }
 }
