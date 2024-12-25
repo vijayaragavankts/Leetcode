0a267d913f6b9a19434803da6410573c58c71dde
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-12-25 13:30:35 +0530

    Time: 4 ms (99.73%) | Memory: 45.1 MB (86.75%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,31 +1,22 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int n = s.length();
-        int l = 0;
-        int r = n-1;
-        int k = 1;
-        while(l <= r){
-            if(s.charAt(l) != s.charAt(r) && k == 0){
-                return false;
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return isPalindrome(s, l + 1, r) || isPalindrome(s, l, r - 1);
             }
-            if(s.charAt(l) == s.charAt(r)){
-                l++;
-                r--;
-            }
-            else if(s.charAt(l) != s.charAt(r) && k == 1){
-                return isPalindrome(s,l+1,r) || isPalindrome(s,l,r-1);
-            }
+            l++;
+            r--;
         }
+
         return true;
     }
-    public boolean isPalindrome(String s, int l, int r){
-        while(l < r){
-            if(s.charAt(l) != s.charAt(r)){
+
+    private boolean isPalindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l++) != s.charAt(r--)) {
                 return false;
-            }
-            else{
-                l++;
-                r--;
             }
         }
         return true;
