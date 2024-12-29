ddf19fc9823fe3111a7a4e57ee0f96ab0f99271b
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-12-29 12:20:51 +0530

    Time: 1 ms (100.00%) | Memory: 45.2 MB (100.00%) - LeetSync

---
+++ 3691-minimum-operations-to-make-columns-strictly-increasing/minimum-operations-to-make-columns-strictly-increasing.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumOperations(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int res = 0;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(grid[j][i] <= grid[j-1][i]){
+                    int diff = grid[j-1][i] - grid[j][i] + 1;
+                    res += diff;
+                    grid[j][i] += diff;
+                }
+            }
+        }
+        return res;
+    }
+}
