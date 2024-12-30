d119e13c3a9398f195a8e811f0117743a9d971b0
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2024-12-30 23:10:09 +0530

    Time: 2 ms (100.00%) | Memory: 57.7 MB (74.47%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
@@ -1,75 +1,43 @@
 class Solution {
     public int minimumDeletions(int[] nums) {
         int n = nums.length;
-        int smallest = Integer.MAX_VALUE;
-        int largest = Integer.MIN_VALUE;
-        for(int val:nums){
-            if(val < smallest){
-                smallest = val;
-            }
-            if(val > largest){
-                largest = val;
-            }
-        }
-        // traverse in both front and back to find those elements
-        int search_small_front = 0;
-        int search_big_front = 0;
-        int search_small_back = 0;
-        int search_big_back = 0;
-        for(int val:nums){
-            search_small_front++;
-            if(val == smallest){
-                break;
-            }
-        }
-        for(int val:nums){
-            search_big_front++;
-            if(val == largest){
-                break;
-            }
-        }
-        for(int i=n-1;i>=0;i--){
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int max_idx = -1;
+        int min_idx = -1;
+        for(int i=0;i<n;i++){
             int val = nums[i];
-            search_small_back++;
-            if(val == smallest){
-                break;
+            if(val > max){
+                max_idx = i;
+                max = val;
             }
-        }
-        for(int i=n-1;i>=0;i--){
-            int val = nums[i];
-            search_big_back++;
-            if(val == largest){
-                break;
+            if(val < min){
+                min_idx = i;
+                min = val;
             }
         }
         int res = 0;
-        int front = 0;
-        if(search_small_front < search_big_front){
-            front = search_big_front;
+
+        if (min_idx > max_idx) {
+            int temp = min_idx;
+            min_idx = max_idx;
+            max_idx = temp;
         }
-        else{
-            front = search_small_front;
-        }
-        int back = 0;
-        if(search_small_back < search_big_back){
-            back = search_big_back;
-        }
-        else{
-            back = search_small_back;
-        }
-        if(search_small_back < search_small_front){
-            res += search_small_back;
-        }
-        else{
-            res += search_small_front;
-        }
-        if(search_big_back < search_big_front){
-            res += search_big_back;
-        }
-        else{
-            res += search_big_front;
-        }
-        return Math.min(res,Math.min(front,back));
+
+
+        // scenario 1 : Front
+
+        int res1 = max_idx + 1;
+
+        // scenario 2 : Back
+
+        int res2 = n - min_idx;
+
+        // scenario 3 : One from front and One from back
+
+        int res3 = min_idx + 1 + n - max_idx;
+
+        return Math.min(res1,Math.min(res2,res3));
 
     }
 }
