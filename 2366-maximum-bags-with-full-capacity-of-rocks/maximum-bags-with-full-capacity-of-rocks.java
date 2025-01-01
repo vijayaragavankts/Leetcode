dd90bac7b2c2097f37d4f672742a53a000d00eaf
Vijayaragavan <95960286+vijayaragavankts@users.noreply.github.com>
2025-01-01 13:09:19 +0530

    Time: 15 ms (55.36%) | Memory: 57 MB (14.29%) - LeetSync

--- 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.java
+++ 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.java
@@ -6,15 +6,13 @@ class Solution {
             diff[i] = capacity[i] - rocks[i];
         }
         Arrays.sort(diff);
-        int temp = additionalRocks;
-        System.out.println(Arrays.toString(diff));
         int res = 0;
         for(int i=0;i<n;i++){
             if(diff[i] == 0){
                 res++;
             }
-            else if(diff[i] <= temp){
-                temp -= diff[i];
+            else if(diff[i] <= additionalRocks){
+                additionalRocks -= diff[i];
                 res++;
             }
         }
